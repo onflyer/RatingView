ba9efd720ef82bf31472fabfd7e49a9ce8d0053c
onflyer <salkaorama@gmail.com>
2023-11-12 23:36:58 +0100

    Initial Commit

---
+++ RatingView/RatingViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  RatingViewApp.swift
+//  RatingView
+//
+//  Created by Aleksandar Milidrag on 11/12/23.
+//
+
+import SwiftUI
+
+@main
+struct RatingViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
